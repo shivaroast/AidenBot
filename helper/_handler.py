c9d4255b6f6d79a1cc409488bb8b208bdd19c829
shivaroast <46077976+shivaroast@users.noreply.github.com>
2019-01-06 11:19:55 -0800

    Update _handler.py

--- helper/_handler.py
+++ helper/_handler.py
@@ -5,8 +5,7 @@ Command handler module for AidenBot
 from functools import partial as pt
 from . import (
     cat_wrap, convert, curx_wrap, combine, echo, shout,
-    mock, space, aesthetic, bawl1, bawl2, is_palindrome, rng, rpick, emote,
-    translate, isup calc, ask, mirror_toggle, define,
+    mock, space, aesthetic, bawl1, bawl2, is_palindrome, rng, rpick, emote, translate, isup calc, ask, mirror_toggle, define,
     reddit_hot, slap, stalkig_wrap, stalktwt, urban, wiki_get, wiki_lang,
     wolfram, wolfram_wrap, weather
 )
