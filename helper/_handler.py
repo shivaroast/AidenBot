5e51123d378ed60a39f5782dd1cae8148d9843f5
shivaroast <46077976+shivaroast@users.noreply.github.com>
2019-01-11 18:48:35 -0800

    Update _handler.py

--- helper/_handler.py
+++ helper/_handler.py
@@ -7,7 +7,7 @@ from . import (
     about, cat_wrap, convert, curx_wrap, combine, echo, shout,
     mock, space, aesthetic, bawl1, bawl2, is_palindrome, rng, rpick, emote,
     translate, isup, calc, ask,
-    define, reddit_hot, slap, stalkig_wrap, stalktwt,
+    define, reddit_hot, roast, slap, stalkig_wrap, stalktwt,
     ticket_add, ticket_rem, ticket_get, surprise_wrap, urban, wiki_get,
     wiki_lang, wolfram, wolfram_wrap, weather
 )
@@ -24,7 +24,7 @@ HELP_MSG = ("Available commands:\n"
             "ask, cur, calc, isup, mirror, pick, rng, stalk, weather\n"
             "\n"
             "\U0010002D [chat enhancer]:\n"
-            "cat, meme, slap, text\n"
+            "cat, slap, roast, text\n"
             "\n"
             "Use /help <command> for more information. Example: /help wiki")
 
@@ -219,6 +219,10 @@ CMD_HELP = {'about': "Usage: /about\n"
                     "in range <floor>..<ceiling> (inclusive).\n"
                     "<floor> is optional, default is 1.\n"
                     "Example: /rngf 2.19 4.2",
+            
+            'roast': "Usage: /roast <someone>\n"
+                     "Roast <someone>.\n"
+                     "Example: /roast bam",
 
             'shout': "Usage: /shout <something>\n"
                      "REPEAT <SOMETHING> IN UPPERCASE.\n"
@@ -371,6 +375,7 @@ def command_handler(text, user, myself, set_id):
                    'pal': is_palindrome,
                    'ppal': pt(is_palindrome, perfect=True),
                    'pick': rpick,
+                   'roast': roast,
                    'rtix': pt(ticket_rem, allowed=itsme),
                    'shout': shout,
                    'slap': pt(slap, user, myself=myself),
